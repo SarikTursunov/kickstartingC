300a949921f965539e77e0ecdcb520e84b6e98a7
Sarik Tursunov <tsa20000901@mail.ru>
2025-07-10 00:19:31 -0400

    Update kickstarting.c

---
+++ kickstarting.c
@@ -0,0 +1,12 @@
+// Working solo in C projects and addint them into Github and explaining in ReadMe file
+// Today's task is to understand integers int, float and double
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    return 0;
+}
